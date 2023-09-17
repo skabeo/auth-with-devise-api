94cf1f15ef18c9a4d574f7c7887dfdc4cbaba78e
skabeo <spencerokyere95@gmail.com>
2023-09-17 13:35:23 +0000

    authentication with sessions

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "static#home"
+
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+end
